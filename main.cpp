d5f7a7ce7a7daaad369532f4bc423f27a51de2fc
Abhay Pratap Singh <abhaypratapsingh481833@gmail.com>
2024-04-23 00:38:36 +0530

    comments added

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ class Grid {
     private :
     char displayArray[3][3];
     public :
-        Grid(){
+        Grid(){ // Setting the each item to ' ' to show nothing
             for (int i=0; i<3; i++){
                 for (int j = 0; j<3; j++){
                     displayArray[i][j] = ' ';
@@ -13,7 +13,7 @@ class Grid {
             }
         }
         void fill(int position, int player){
-            switch (position){
+            switch (position){  // Setting the value at the position with the player 0 or 1
                 case 1:
                     displayArray[0][0] = player? 'X' : 'O';
                     break;
@@ -43,18 +43,16 @@ class Grid {
                     break;
             }
         }
-        void show(){
-            
+        void show(){    // Temperary code to show the basic grid of the game
             cout<<endl;
             for (int i=0 ; i<3 ; i++){
-                
                 for (int j = 0 ; j<3 ; j++){
                     cout<<"  "<<displayArray[i][j];
-                    if (j!=2){
+                    if (j!=2){  // print : so that 1 , 2 , 3 element in the row is seperated
                         cout<<"  :";
                     }
                 }
-                if (i!=2){
+                if (i!=2){  // print after 1st and 2nd row of grid so it is betwwen the 1,2,3 rows
                     cout<<endl<<" ..............."<<   endl;
                 }
             }
