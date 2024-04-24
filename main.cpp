a86dd4c8f3ea694f5db9d232d9f665a6f5d69f62
Abhay Pratap Singh <abhaypratapsingh481833@gmail.com>
2024-04-24 12:07:18 +0530

    imporve show func to 5*5 for each block

--- main.cpp
+++ main.cpp
@@ -6,35 +6,67 @@ using namespace std;
 
 class Grid {
     protected :
-        char displayArray[3][3];
+        int gridSize;
+        char positionsArray[3][3];
+        char displayArray[15][15];
     public :
         Grid(){ // Setting the each item to ' ' to show nothing
+            gridSize = 5;
+            memset(positionsArray , ' ',sizeof(positionsArray));
             memset(displayArray , ' ',sizeof(displayArray));
         }
         bool fill(int posX, int posY, int player){
-            if (displayArray[posX][posY] == ' '){
-                displayArray[posX][posY] = player?'X':'O';  // setting player piece to the specific location
-                return 1;
-            }
-            else {
-                return 0;
-            }
-        }
-        
-        void show(char* statement){    // Temperary code to show the basic grid of the game
-            system("cls");
-            cout<<endl;
-            for (int i=0 ; i<3 ; i++){
-                for (int j = 0 ; j<3 ; j++){
-                    cout<<"  "<<displayArray[i][j];
-                    if (j!=2){  // print : so that 1 , 2 , 3 element in the row is seperated
-                        cout<<"  :";
+            cout<<posX<<" s "<<posY<<endl;
+            if (positionsArray[posX][posY] == ' '){
+                for (int i=posX*gridSize; i<posX*gridSize+gridSize; i++){
+                    for (int j = posY*gridSize; j<posY*gridSize+gridSize; j++){     
+                        if (player){  // filling X in the X pattern
+                            if (i - posX*gridSize == j - posY*gridSize){  // find each location (between 0 - gridSize) in selected block of grid then turn 'X' digonally
+                                displayArray[i][j]='X';
+                            }
+                            if(i - posX*gridSize == (gridSize - (j - posY*gridSize + 1))){   //  setting 'X' to location where (0  = gridSize - 0 - 1) digonal from other side 
+                                displayArray[i][j]='X';
+                            }
+                        }
+                        else if(!(player) && (i==posX*gridSize || i==posX*gridSize + gridSize - 1 || j==posY*gridSize || j==posY*gridSize + gridSize - 1 )) {   // setting the position of outside||border of the grid with 'O' if player is 'O'
+                            displayArray[i][j] = '0';
+                        }
                     }
                 }
-                if (i!=2){  // print after 1st and 2nd row of grid so it is betwwen the 1,2,3 rows
-                    cout<<endl<<" ..............."<<   endl;
-                }
+                positionsArray[posX][posY] = player?'X':'O';  // setting player piece to the specific location
+                return 1;
             }
+            return 0;
+        }
+        void showDis(char* statement){
+            for (int i=0; i<gridSize*3 ; i++){
+                for(int j =0 ; j<gridSize*3 ; j++){
+                    if (j%gridSize == 0){
+                        cout<<"||";
+                    }
+                    cout<<displayArray[i][j];
+                }
+                cout<<endl;
+            }
+            cout<<endl<<endl<<endl<<endl;
+            cout<<statement<<endl;
+        }
+        void show(char* statement){    // Temperary code to show the basic grid of the game
+            system("cls");
+            cout<<"Started"<<endl;
+            for (int i=0 ; i<3*gridSize ; i++){
+                if (i%gridSize == 0 && i!= 0){
+                    cout<<"------------------------"<<endl; // Giving horizontal line for the better look
+                }
+                for (int j = 0 ; j<3*gridSize ; j++){
+                    if(j%gridSize == 0  && j!= 0){
+                        cout<<" | ";   // giving vertical look for better view
+                    }
+                    cout<<displayArray[i][j];
+                }
+                cout<<endl;                
+            }
+            cout<<"Ended"<<endl;
             cout<<endl<<endl<<endl<<endl;
             cout<<statement<<endl;
         }
@@ -47,7 +79,6 @@ class play : public Grid{
             char statement[300] = "O Player turn";  // statement to print in the show function
             int x,y;    // temperary way to take move input
             int player = 0;
-            this->show(statement);
             while(!checkWin()){
                 this->show(statement);
                 cin>>x>>y;  // temperary way to take move input
@@ -66,21 +97,21 @@ class play : public Grid{
         }
         bool checkWin(){
             for (int i = 0 ; i<3 ; i++){
-                if ( displayArray[i][0] != ' ' && ((displayArray[i][0] == displayArray[i][1]) && (displayArray[i][1] == displayArray[i][2]))){
-                    winner = displayArray[i][0];
+                if ( positionsArray[i][0] != ' ' && ((positionsArray[i][0] == positionsArray[i][1]) && (positionsArray[i][1] == positionsArray[i][2]))){
+                    winner = positionsArray[i][0];
                     return 1;
                 }
-                else if (displayArray[0][i] != ' ' &&  (displayArray[0][i] == displayArray[1][i]) && (displayArray[1][i] == displayArray[2][i])){
-                    winner = displayArray[0][i];
+                else if (positionsArray[0][i] != ' ' &&  (positionsArray[0][i] == positionsArray[1][i]) && (positionsArray[1][i] == positionsArray[2][i])){
+                    winner = positionsArray[0][i];
                     return 1;
                 }
             }
-            if ( displayArray[0][0] != ' ' &&  (displayArray[0][0] == displayArray[1][1]) && (displayArray[1][1] == displayArray[2][2])){
-                winner = displayArray[0][0];
+            if ( positionsArray[0][0] != ' ' &&  (positionsArray[0][0] == positionsArray[1][1]) && (positionsArray[1][1] == positionsArray[2][2])){
+                winner = positionsArray[0][0];
                 return 1;
             }
-            else if( displayArray[0][2] != ' ' && (displayArray[0][2] == displayArray[1][1]) && (displayArray[1][1] == displayArray[2][0])){
-                winner = displayArray[0][2];
+            else if( positionsArray[0][2] != ' ' && (positionsArray[0][2] == positionsArray[1][1]) && (positionsArray[1][1] == positionsArray[2][0])){
+                winner = positionsArray[0][2];
                 return 1;
             }
             return 0;
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+Grid a;
+    // a.showDis();
+    // for (int i = 0; i<3 ; i++){
+    //     for (int j = 0; j<3 ; j++){
+    //         a.fill(i ,j ,1);
+    //         a.showDis();
+    //     }
+    
